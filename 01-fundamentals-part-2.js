4a035a8f7da955ff62ee5563c658692420ec94d0
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-11-03 19:39:13 -0400

    fuction declarations and expressions

--- 01-fundamentals-part-2.js
+++ 01-fundamentals-part-2.js
@@ -22,14 +22,35 @@
 // logger();
 // logger();
 
-function fruitProcessor(apples, oranges) {
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
+// function fruitProcessor(apples, oranges) {
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice)
+// // console.log(fruitProcessor(5, 0)) // altrnate way
+
+// const appleOrangeJuice = fruitProcessor(2, 4)
+// console.log(appleOrangeJuice)
+
+
+
+//======FUNCTION DECLARATIONS VS EXPRESSIONS ========
+
+//fuction declaration
+function calcAge1(birthYear) {
+    return 2021 - birthYear
 }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice)
-// console.log(fruitProcessor(5, 0)) // altrnate way
+const age1 = calcAge1(1975)
+console.log(age1, 'age 1')
 
-const appleOrangeJuice = fruitProcessor(2, 4)
-console.log(appleOrangeJuice)
+
+//function expression (expression produce values)
+const calcAge2 = function (birthYear) {
+    return 2021 - birthYear
+}
+
+const age2 = calcAge2(1975)
+console.log(age2, 'age 2')
