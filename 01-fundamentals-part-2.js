cefae5cec12e0e2a040e727bdda2e342249c3853
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-11-03 19:58:33 -0400

    arrow functions

--- 01-fundamentals-part-2.js
+++ 01-fundamentals-part-2.js
@@ -39,18 +39,37 @@
 //======FUNCTION DECLARATIONS VS EXPRESSIONS ========
 
 //fuction declaration
-function calcAge1(birthYear) {
-    return 2021 - birthYear
+// function calcAge1(birthYear) {
+//     return 2021 - birthYear
+// }
+
+// const age1 = calcAge1(1975)
+// console.log(age1, 'age 1')
+
+
+// //function expression (expression produce values... and values can be tied to a variable)
+// const calcAge2 = function (birthYear) {
+//     return 2021 - birthYear
+// }
+
+// const age2 = calcAge2(1975)
+// console.log(age2, 'age 2')
+
+
+
+// ======= ARROW FUNCTIONS ========
+
+const calcAge3 = birthYear => 2021 - birthYear
+const age3 = calcAge3(1975)
+console.log(age3)
+
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037 - birthYear
+    const retirement = 65 - age
+    // return retirement
+    return `${firstName} retires in ${retirement} years`
 }
 
-const age1 = calcAge1(1975)
-console.log(age1, 'age 1')
-
-
-//function expression (expression produce values... and values can be tied to a variable)
-const calcAge2 = function (birthYear) {
-    return 2021 - birthYear
-}
-
-const age2 = calcAge2(1975)
-console.log(age2, 'age 2')
+console.log(yearsUntilRetirement(1991, 'Jonas'))
+console.log(yearsUntilRetirement(1980, 'Ethan'))
