d7611d09542b00f7c58f297e0714f942fa2d33da
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-11-03 19:26:26 -0400

    functions

--- 01-fundamentals-part-2.js
+++ 01-fundamentals-part-2.js
@@ -1,7 +1,35 @@
 'use strict'; // must be first line in order to use.
 
-let hasDriversLicense = false;
-const passTest = true;
+// ACTIVATING STRICT MODE (ABOVE)
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive')
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive')
+
+// const interface = 'Audio' // reserved word error
+// const private = 534  // reserved work error
+
+
+//======FUNCTIONS=======
+
+// function logger() {
+//     console.log('My name is Jonas');
+// }
+
+// // calling / running / invoking
+// logger();
+// logger();
+// logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice)
+// console.log(fruitProcessor(5, 0)) // altrnate way
+
+const appleOrangeJuice = fruitProcessor(2, 4)
+console.log(appleOrangeJuice)
