956c3c3dbeaab67fbeb423411462f7a4b569c040
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-11-29 19:27:51 -0500

    array challenge

--- 01-fundamentals-part-2.js
+++ 01-fundamentals-part-2.js
@@ -286,3 +286,37 @@
 // } else {
 //     console.log('You dont have any friends')
 // }
+
+// ==== CODE CHALLENGE =====
+
+
+// const bills = [125, 555, 44]
+// const tips = []
+// const totals = []
+
+// function calcTip(billValue) {
+//     if(billValue >= 50 && billValue <= 300){
+//         totals.push(billValue + (billValue* .15))
+//         tips.push(billValue * .15)
+//         console.log(`A 15% tip is ${billValue * .15}`)
+//     } else {
+//         totals.push(billValue + (billValue* .20))
+//         tips.push(billValue * .20)
+//         console.log(`A 20% tip is ${billValue * .20}`)
+//     }
+// }
+// calcTip(bills[0])
+// console.log('Bills', bills)
+// console.log('Tips', tips)
+// console.log('Totals', totals)
+
+
+const calcTip = function(bill) {
+    return bill >= 50 && bill <= 300 ? bill * .15 : bill * .20
+}
+
+const bills = [125, 555, 44]
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip([2])]
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+
+console.log(bills, tips, totals)
